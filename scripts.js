5dd9268afcc3279aa1f7160338126d6e422865e8
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-05-24 10:32:13 +0200

    Log the array of name lengths

--- scripts.js
+++ scripts.js
@@ -56,3 +56,6 @@ console.log(names.map((name, index) => `${name} (${provinces[index]})`));
 
 // Logs the new array of province names in all uppercase
 console.log(provinces.map(province => province.toUpperCase()));
+
+// Logs the array of name lengths
+console.log(names.map(name => name.length));
